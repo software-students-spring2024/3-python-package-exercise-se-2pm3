368a156b234456adc2847e112c8e7f0230446643
minjae07206 <minjaekor@gmail.com>
2024-03-25 23:02:20 +0400

    fixed unit test for randomDice

--- src/pyrandom2pm/randomDice.py
+++ src/pyrandom2pm/randomDice.py
@@ -1,3 +1,7 @@
-def dummy():
-    print("Hello")
-    return 123
+import random
+def randomDice(num_dice, num_sides):
+    total = 0
+    for i in range(num_dice):
+        random_number = random.randint(1, num_sides)
+        total += random_number
+    return total
