2639e2cedb2efc04774a7d982224396afa4f95f1
yl7408 <yl7408@nyu.edu>
2024-03-25 12:23:50 -0700

    8ball done

---
+++ src/pyrandom2pm/random8ball.py
@@ -0,0 +1,19 @@
+import random
+def random8ball(str):
+    try:
+        if '?' in str:
+            ram = random.randint(0, 3)
+            if ram ==0:
+                temp = "No"
+                return temp
+            elif ram ==1:
+                temp = "Yes"
+                return temp
+            else: 
+                return "I don't know."
+        else:
+            return "That was not the question."
+    except TypeError:
+        return "I don't understand beyond string."
+
+    
