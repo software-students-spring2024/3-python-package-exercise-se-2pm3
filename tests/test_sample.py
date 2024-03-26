2f2de1da4e07c188b07ae0f3ffbecfe3d9782485
yl7408 <yl7408@nyu.edu>
2024-03-25 23:23:04 -0700

    possible fix

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -27,13 +27,16 @@ class Tests:
     #         randomDice.randomDice(1, None)
     #     with pytest.raises(ValueError):
     #         randomDice.randomDice(None, 6)
-    def test_randomDice_num_dice_invalid(self):
+    def test_randomDice_value_invalid(self):
         with pytest.raises(ValueError):
             randomDice.randomDice(0, 6)  # Should raise ValueError for num_dice = 0
         with pytest.raises(ValueError):
             randomDice.randomDice(3, 6)  # Should raise ValueError for num_dice > 2
         with pytest.raises(ValueError): # Should raise ValueError when all parameters are zero
             randomDice.randomDice(0, 0)
+        with pytest.raises(ValueError):# Should raise ValueError for num_dice ==0
+            randomDice.randomDice(1, 0)
+    def test_randomDice_invalid_type(self):
         with pytest.raises(TypeError): # Should raise TypeError when any or both of the parameter are not integer
             randomDice.randomDice("Hello", "World")
         with pytest.raises(TypeError): # Should raise TypeError when any or both of the parameter are not integer
@@ -46,9 +49,7 @@ class Tests:
             randomDice.randomDice(248.88, 3)
         with pytest.raises(TypeError): # Should raise TypeError when any or both of the parameter are not integer
             randomDice.randomDice(1, 232434.2323)
-    def test_randomDice_invalid_num_sides(self):
-        with pytest.raises(ValueError):# Should raise ValueError for num_dice ==0
-            randomDice.randomDice(1, 0)
+
         
 
         
@@ -57,11 +58,12 @@ class Tests:
         result = randomNum.randomNum(num)  # Call the function with the correct parameters
         assert isinstance(result, int)
         assert 1 <= result <= num
-    def test_randomNum_invalid(self):
+    def test_randomNum_value_invalid(self):
         with pytest.raises(ValueError):
             randomNum.randomNum(-1) 
         with pytest.raises(ValueError):
             randomNum.randomNum(0)  # Should raise ValueError for num = 0
+    def test_randomNum_type_invalid(self):
         with pytest.raises(TypeError):
             randomNum.randomNum("Hello")  # Should raise TypeError for the input being string
         with pytest.raises(TypeError):
@@ -71,14 +73,15 @@ class Tests:
         result = randomCoin.randomCoin(num_flip)  # Call the function with the correct parameters
         assert isinstance(result, str)
         assert result is not None
-    def test_randomCoin_invalid_input(self):
+    def test_randomCoin_invalid_value_input(self):
         with pytest.raises(ValueError):
             randomCoin.randomCoin(0)  # Should raise ValueError for num_flip = 0
         with pytest.raises(ValueError):
             randomCoin.randomCoin(-200)  # Should raise ValueError for num_flip <0
-        with pytest.raises(ValueError):
+    def test_randomCoin_invalid_type_input(self):
+        with pytest.raises(TypeError):
             randomCoin.randomCoin("Hello")  # Should raise TypeError for the input being string
-        with pytest.raises(ValueError):
+        with pytest.raises(TypeError):
             randomCoin.randomCoin(23.333)  # Should raise TypeError for the input being float
     def test_random8ball_valid_input(self):
         strn = "Hello, World?"
